5c9db8e33bdcee5ced1bdcb0c913b31743febd4f
Sumio K <tap.miwo@gmail.com>
2022-10-28 05:52:49 +0900

    routeing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get "homes/top"
   get "/" => "homes#top"
 
+  post "posts/create" => "posts#create"
+  post "posts/destroy" => "posts#destroy"
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
-  post "posts/create" => "posts#create"
   get "posts/show" => "posts#show"
 
   get "users/new" => "users#new"
