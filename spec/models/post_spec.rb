e536169da9a603dd0b56aef1e489757c2ef304ef
Sumio K <tap.miwo@gmail.com>
2022-10-29 08:13:45 +0900

    model test done

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,31 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :moddel do
   
-  it "投稿ができる"
+  it "投稿ができる" do
+    user = User.create(
+      id: 5,
+      name: "sample5",
+      email: "sample5@example.com",
+      password: "sample5"
+    )
+    post = Post.new(
+      user_id: 5,
+      content: "test"
+    )
+    expect(post).to be_valid
+  end
 
-  it "コンテンツが空の投稿はできない"
+  it "コンテンツが空の投稿はできない"do
+    user = User.create(
+      id: 6,
+      name: "sample6",
+      email: "sample6@example.com",
+      password: "sample6"
+    )
+    post = Post.new(
+      user_id: 6
+    )
+    expect(post).to be_invalid
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  it "正常にユーザー登録が可能"
+  describe 'valid' do
+    it "正常にユーザー登録が可能" do
+      user = User.new(
+        name: "sample",
+        email: "sample@example.com",
+        password: "sample"
+      )
+      user.save
+      expect(user).to be_valid
+    end
 
-  it "ユーザー名が空の場合は登録できない"
+    it "ユーザー名が空の場合は登録できない" do
+      user = User.new(
+        email: "sample2@example.com",
+        password: "sample2"
+      )
+      expect(user).to be_invalid
+    end
 
-  it "Emailが空の場合は登録できない"
+    it "Emailが空の場合は登録できない" do
+      user = User.new(
+        name: "sample3",
+        password: "sample3"
+      )
+      expect(user).to be_invalid
+    end
 
-  it "パスワードが空の場合は登録できない"
-
-  it "パスワードと確認パスワードが一致しない場合は登録できない"
-
-  it "メールアドレスに＠が含まれない場合登録できない"
-
-  it "重複したメールアドレスは使用できない"
+    it "パスワードが空の場合は登録できない" do
+      user = User.new(
+        name: "sample4",
+        email: "sample4@example.com"
+      )
+      expect(user).to be_invalid
+    end
+  end
 end
