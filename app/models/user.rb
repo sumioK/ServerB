d90154e9a36eef91ed9aeffb18c61fdd0c97699a
Sumio K <tap.miwo@gmail.com>
2022-10-28 21:59:36 +0900

    valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true ,uniqueness: true, format: {with: /@/,message:"不正なメールアドレスです"}
+  validates :password, presence:true
   has_many :users, dependent: :destroy
 end
